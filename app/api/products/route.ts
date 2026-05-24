b6e80555038a830a2f55634b41392d9fda40cbbf
sanjana-1025 <kolavennusanjana@gmail.com>
2026-05-24 21:58:32 +0530

    fixed prisma import

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,30 +1,22 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const inventory = await prisma.inventory.findMany({
-      include: {
-        product: true,
-        warehouse: true,
-      },
-    });
 
-    const formatted = inventory.map((item) => ({
-      productId: item.product.id,
-      productName: item.product.name,
-      warehouseId: item.warehouse.id,
-      warehouseName: item.warehouse.name,
-      availableStock: item.availableStock,
-    }));
+  const inventory = await prisma.inventory.findMany({
+    include: {
+      product: true,
+      warehouse: true,
+    },
+  });
 
-    return NextResponse.json(formatted);
-  } catch (error) {
-    console.error(error);
+  const formatted = inventory.map((item) => ({
+    productId: item.product.id,
+    productName: item.product.name,
+    warehouseId: item.warehouse.id,
+    warehouseName: item.warehouse.name,
+    availableStock: item.totalStock - item.reservedStock,
+  }));
 
-    return NextResponse.json(
-      { error: "Failed to fetch products" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json(formatted);
 }
